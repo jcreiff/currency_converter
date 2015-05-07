ea2349892f53edd152382542521200bb9c9dbb09
Joseph Reiff <jreiff@Josephs-Air.att.net>
2015-05-07 19:04:24 -0400

    create get_amount and get_code methods in currency class

--- currency.rb
+++ currency.rb
@@ -3,4 +3,12 @@ class Currency
     @amount = amount
     @code = code
   end
+
+  def get_amount
+    @amount
+  end
+
+  def get_code
+    @code
+  end
 end
--- test.rb
+++ test.rb
@@ -3,3 +3,6 @@ require './currency.rb'
 us_dollar = Currency.new(50, "USD")
 
 p us_dollar
+
+puts us_dollar.get_amount
+puts us_dollar.get_code
